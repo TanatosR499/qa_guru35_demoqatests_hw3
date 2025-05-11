c79bb7b5a74e32fefc2227e624bef27ddbc83a6b
Tatiana Chigorina <tanya199613@gmail.com>
2025-05-11 15:06:31 +0500

    add faker

---
+++ src/test/java/utils/DateUtils.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DateUtils {
+    static String dottedLocaleDatePattern = "dd.MM.yyyy";
+
+    public String getDateInString(Date date, String pattern) {
+        SimpleDateFormat df = new SimpleDateFormat(pattern);
+        return df.format(date);
+    }
+
+    public String getDayFromDate(Date date) {
+        String dt = getDateInString(date, dottedLocaleDatePattern);
+        return dt.substring(0, 2);
+    }
+
+    public Integer getMonthValueFromDate(Date date) {
+        LocalDate localDate = LocalDate.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        return localDate.getMonthValue();
+    }
+
+    public String getYearFromDate(Date date) {
+        String dt = getDateInString(date, dottedLocaleDatePattern);
+        return dt.substring(6);
+    }
+
+    public String getMonthFromDate(Date date) {
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        int monthNumber = getMonthValueFromDate(date);
+        return months[monthNumber - 1];
+    }
+}
